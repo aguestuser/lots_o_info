2eb5851ad9888a74c36d6c8a65ea141d6786c94b
Austin Guest <guest.austin@gmail.com>
2014-11-08 20:53:09 -0500

    refactored pluto parser to remove mutations, local state in parser object

--- parse/parser.js
+++ parse/parser.js
@@ -6,101 +6,91 @@
 var fs = require('fs')
   _ = require('underscore')
 
-module.exports = function(spec){
-  //input: Object Literal of form:
-    // { 
-    //  data_set: Enum ({'pluto', 'acris', 'dob' }, 
-    //  source: Str (filepath) 
-    // }
-  //output: Parser obj
+module.exports = (function(){
 
-  //ATTRIBUTES
-  var that = spec
-  that.translations = require('./' + that.data_set + '_translations')
+  var that = {}
 
   //PUBLIC METHODS
-  var read_file = function(callback){
-    // input: initialized Parser object(implicit), Function(data) [CPS]
+  var build_matrix = function(source_path, callback){
+    // input: Str(filepath), Function(Arr of Arrs) [CPS]
     // does: parses csv data to 2d matrix, adds matrix attribute to parser object
-    // output: mutated Parser object
 
-    fs.readFile(that.source, 'utf8', function(err, data){
-      that['matrix'] = matrix_from(data)
-      callback.apply(that)
+    fs.readFile(source_path, 'utf8', function(err, data){
+      callback( to_matrix( data ) )
     })
   }
-  that.read_file = read_file
+  that.build_matrix = build_matrix
 
-  var build_documents = function(callback){
-    // input: Parser object with matrix attribute initialized
-    // does: constructs doc attribute
-    // output: mutated Parser object
-
-    docs = []
-    _.each(that.matrix, function(row){
-      docs.push(document_from(row)) 
-    })
-
-    that['docs'] = docs
-    callback.apply(that)
+  var translations = function(data_set){
+    return require('./' + data_set + '_translations')
   }
-  that.build_documents = build_documents 
+  that.translations = translations
+
+  var build_documents = function(matrix, translations){
+    // input: Arr of Arrs, JSON Obj
+    // does: translates matrix to arr of json docs
+    // output: Arr of JSON Objs
+
+    return matrix.map(function(row){
+      return to_doc(row, translations)
+    })
+  }
+  that.build_documents = build_documents
 
   //PRIVATE METHODS
 
-  function matrix_from(data){
+  function to_matrix(data){
     //input: Str (results of fs.ReadFile)
     //output: Arr of Arr of Strs (each Str is a csv cell value)
-    var rows = data.split('\r')
-    rows.shift()
-    return _.map(rows, function(row){
-      return row.split(',') 
-    })
+    return _.rest(data.split('\r'))
+      .map(function(row){
+        return row.split(',')
+      })
   }
 
-  function document_from(row){
-    var doc = {}
-    _.each(that.translations, function(val, key){
-      doc[key] = nested_values_from(val, row)
-    })
-    return doc
+  function to_doc(row, translations){
+    //input: Arr of Strs
+    //output: Nested JSON object
+    return _.object(
+      _.map(translations, function(val, key){
+        return [ key, get_nested_values(val, row) ]
+      })
+    ) 
   }
 
-  function nested_values_from(val, row){
+  function get_nested_values(val, row){
     // input: Object Literal, String
     // output: Nested JSON objects (or String if val is String)
 
     if ( _.isArray(val) ){ // for array
-      return arr_from(val, row) // (will recurse)
+      return get_arr(val, row) // (will recurse)
     } 
     else { // for object
       if (val.hasOwnProperty('index')){
         return val.cast(row[val.index]) // break recursion
       }
       else {
-        return obj_from(val, row) // (will recurse)  
+        return get_obj(val, row) // (will recurse)  
       }
     }
   }
 
-  function arr_from(val, row){
-    var arr = []
-    _.each(val, function(elem){
-      arr.push( nested_values_from( elem, row ) ) // recurse
+  function get_arr(val, row){
+    return _.map(val, function(elem){
+      return get_nested_values( elem, row )  // recurse
     })
-    return arr
   }
 
-  function obj_from(val, row){
-    var obj = {}
-    _.each(val, function(sub_val, key){
-      obj[key] = nested_values_from(sub_val, row) // recurse
-    })
-    return obj
+  function get_obj(val, row){
+    return _.object(
+      _.map(val, function(sub_val, key){
+        return [ key, get_nested_values(sub_val, row) ]
+      })
+    )
   }
 
   return that
-}
+})();
 
 
 
--- test/parser_tests.js
+++ test/parser_tests.js
@@ -1,43 +1,53 @@
-var build_parser = require ('../parse/parser')
+var parser = require ('../parse/parser')
   , should = require('should')
   , fs = require('fs')
 
 describe('Parser', function(){
 
   describe('Pluto', function(){
-    var parser = build_parser({ 
-          source: './test/sample_data/pluto/BK.csv',
-          data_set: 'pluto'  
-        })
+    var data_set = 'pluto'
+      , source_path = './test/sample_data/pluto/BK.csv'
       , expected = require('./support/parse_pluto_expected')
+      , matrix, docs
 
-    describe('construction', function(){
+    // describe('construction', function(){
       
-      it('retrieves correct translations object', function(){
-        parser.translations.should.eql( require('../parse/pluto_translations') )
-      })
-    })
+    //   it('retrieves correct translations object', function(){
+    //     parser.translations.should.eql( require('../parse/pluto_translations') )
+    //   })
+    // })
 
-    describe('#read_file()', function(){
+    describe('#build_matrix', function(){
+
       beforeEach(function(done){
-        parser.read_file(done)
+        parser.build_matrix(source_path, function(res){
+          matrix = res
+          done()
+        })
       })
 
       it('correctly parses 2d matrix from csv', function(){
-        parser.matrix.should.eql( expected.matrix )
+        // parser.matrix.should.eql( expected.matrix )
+        matrix.should.eql( expected.matrix )
       })
-    }) 
 
-    describe('#build_documents()', function(){
-      
-      beforeEach(function(done){
-        parser.read_file(function(){
-          parser.build_documents(done)
+      describe('#translations', function(){
+
+        it('retrieves correct translations object', function(){
+          parser.translations('pluto').should.eql( require('../parse/pluto_translations') )
         })
       })
 
-      it('correctly maps cell values to json values', function(){
-        parser.docs.should.eql(expected.docs)
+      describe('#build_documents', function(){
+      
+        beforeEach(function(){
+          var translations = parser.translations('pluto')
+          docs = parser.build_documents(matrix, translations)
+        })
+
+        it('correctly maps cell values to json values', function(){
+          docs.should.eql(expected.docs)
+        })
       })
     }) 
   })
